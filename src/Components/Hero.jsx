84267a4b5229356989f3638fd23a87c419fbf4b6
Joseph Melvin Kanu <kanujosephmelvin@gmail.com>
2025-03-11 01:54:51 +0000

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
                 <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
                     <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
                         <h1 className="text-sm text-[#DA1212] font-medium">
-                            Over 200 successful deals
+                            Over 200 Delicious Recipes Shared
                         </h1>
                         <h2 className="text-4xl text-neutral-200 font-extrabold md:text-5xl">
-                            We help startups to grow and make money
+                            We help food enthusiasts discover and share amazing recipes.
                         </h2>
                         <p className="text-neutral-300">
-                            Discover and explore innovative startups in one place. Connect with emerging businesses and find the next big idea!
+                            Explore and experiment with innovative culinary creations in one place. Connect with home cooks and food lovers, and find your next favorite dish!
                         </p>
                     </div>
                     <div className="flex-none mt-14 md:mt-0 md:max-w-xl">
